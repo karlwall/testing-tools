efa81a7138f2f1aa29f1ca014161d58920c4147e
kdwyer <kdwyer@bonline.com>
2012-01-27 14:02:08 +0000

    Yaml existence and validity checker.
     - checks yaml files against datastore
     - reports failing urls
     - reports files which are not valid yaml

---
+++ xcheck.py
@@ -0,0 +1,158 @@
+#! /usr/bin/env python
+
+"""
+Reports differences between the industries in the datastore and the yaml files
+in repo.
+
+"""
+
+# get industries from $server
+# for each industry, check in $repo
+# -- if yaml exists
+# --- if yaml is valid yaml file
+
+import csv
+import os
+import re
+import subprocess
+import sys
+import tempfile
+import urllib2
+
+import yaml
+
+VERTICAL = "core_vertical"
+INDUSTRY = "core_industry"
+
+def main():
+    """Main function."""
+    try:
+        server = sys.argv[1]
+        repo = sys.argv[2]
+        google_appengine_path = sys.argv[3]
+    except IndexError:
+        usage()
+        return 1
+    download(server, google_appengine_path)
+    verticals = load_data()
+    check(repo, verticals)
+    return 0
+
+def download(server, google_appengine_path):
+    """
+    Downloads data from the app engine datastore and writes to .csv files.
+
+    """
+    for kind in (VERTICAL, INDUSTRY):
+        args = ["python", 
+                "%s" % os.path.join(google_appengine_path, "bulkloader.py"),
+                "--config_file=bulkloader.yaml",
+                "--download", 
+                "--url=http://%s/_ah/remote_api/" % server,
+                "--filename=%s" % _get_csv_path(kind),
+                "--kind=%s" % kind,
+                "--namespace=-global-"] 
+        # run command
+        rc = subprocess.call(args)
+        if rc != 0:
+            raise IOError("Download failed for kind %s" % kind)
+    return
+
+def load_data():
+    """
+    Loads data from .csv files into dictionary.
+
+    """
+    # Build keys: vertical csv file has columns: name, key.
+    map_ = {}
+    data = {}
+    reader = csv.reader(open(_get_csv_path(VERTICAL), "rb"))
+    reader.next()        # Ignore header row
+    for row in reader:
+        data[row[0]] = []
+        map_[row[1]] = row[0]    # Map id to name
+    # Industry csv file has columns: name, key, vertical_id.
+    reader = csv.reader(open(_get_csv_path(INDUSTRY), "rb"))
+    reader.next()
+    for row in reader:
+        key = map_[row[2]]
+        data[key].append(row[0])
+    return data
+
+def check(repo, verticals):
+    """
+    Checks that expected yaml files exist, and that they can be loaded by the
+    yaml module.
+
+    Does not attempt to check that the data is valid for the bOnline
+    application.
+
+    """
+    for vertical in verticals:
+        results = []
+        for industry in verticals[vertical]:
+            url = _get_yaml_url(repo, vertical, industry)
+            try:
+                response = urllib2.urlopen(url)
+            except urllib2.URLError as ex:
+                print "Attempt to fetch yaml file for vertical %s industry %s failed" % (vertical, industry)
+                print "URL was %s" % url
+                print "Exception was: %s" % ex
+                try:
+                    print "Status code was %d" % ex.code
+                except AttributeError:
+                    pass
+                results.append("E")
+                break
+            try:
+                yaml.load(response.read())
+            except yaml.YAMLError as ex:
+                print "Attempt to load yaml file for vertical %s industry %s failed" % (vertical, industry)
+                print "%s" % ex
+                results.append("X")
+                break
+            results.append(".")
+        print "".join(results)
+    return
+
+
+
+
+def usage():
+    print >> sys.stderr, "%s AppEngineServer StaticRepository" % __file__
+
+
+
+
+def _get_csv_path(name):
+    """Gets path to csv file for kind. """
+    return os.path.join(tempfile.gettempdir(), "%s" % (name + ".csv"))
+
+#
+# The _get_yaml_url function is taken from bonline.signup.mapper
+# as the bonline app may not be in sys.path.
+#
+def _get_yaml_url(repo, vertical, industry):
+    folder = slugify(unicode(vertical))
+    filename = slugify(unicode(industry)) + ".yaml"
+    return "http://%s/static/industries/%s/%s" % (repo, folder, filename)
+
+
+# 
+# The slugify function is taken from django.template.defaultfilter.py
+# as django may not be in sys.path
+#
+def slugify(value):
+    """
+    Normalizes string, converts to lowercase, removes non-alpha characters,
+    and converts spaces to hyphens.
+    """
+    import unicodedata
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
+    return re.sub('[-\s]+', '-', value)
+
+
+
+if __name__ == "__main__":
+    sys.exit(main())
